b1f71e63fd97bd8832181556fc1c01cfed795f36
Nimesh D Wijerathna <nimeshdwejerathna2020@gmail.com>
2021-09-11 17:34:30 +0530

    add some styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import Row from './Row'
-import requests from './requests'
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Dinuka Nimesh</h1>
-    <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}/>
+    <div className="app">
+      <Banner />
+      
+
+      <Row
+        title="NETFLIX ORIGINAL"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargerRow
+      />
     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargerRow}) {
 
      const [movies, setMovies] = useState([])
 
@@ -23,8 +23,8 @@ function Row({title, fetchUrl}) {
                <div className="row__posters">
                     {movies.map(movie => (
                          <img key={movie.id} 
-                         className="row__poster"
-                         src={`${base_url}${movie.poster_path}`} 
+                         className={`row__poster ${isLargerRow && 'row__posterLarge'}`}
+                         src={`${base_url}${isLargerRow ? movie.poster_path : movie.backdrop_path }`} 
                          alt={movie.name} />
                     ))}
                </div>
